6c39b8fd2eee51bf916385c2937aa3f2d0f4eed9
Manuel Pereira <ee12314@fe.up.pt>
2017-05-08 15:18:57 +0100

    LogIn/Register com o facebook implementado, mas não ligado ao gui
    é possível que as bibliotecas adicionadas não vão junto com o projeto

--- Chat/src/client/LogIn.java
+++ Chat/src/client/LogIn.java
@@ -2,8 +2,7 @@ package client;
 
 import java.net.InetAddress;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -25,18 +24,17 @@ public class LogIn {
 		ConnectorFile conector = new ConnectorFile();
 
 		try{
-			ResultSet rs = conector.SearchMySQLData("SELECT `password`, `nome`, `num_amigos` FROM utilizador WHERE email = '" + user.getEmail() + "'");
+			ResultSet rs = conector.SearchMySQLData("SELECT `password`, `nome` FROM utilizador WHERE email = '" + user.getEmail() + "'");
 
 			if (!rs.isBeforeFirst()){return -1;}
 			else {
 				rs.next();
 				if(!rs.getString("password").equals(user.getPassword())){return 0;}
 				user.setUserName(rs.getString("nome"));
-				user.setNum_amigos(rs.getInt("num_amigos"));
 				String c = InetAddress.getLocalHost().getHostAddress();
 				conector.AddMySQLData("UPDATE `utilizador` SET `conectado`='online', `IP`='"+ c +"' WHERE email = '"+user.getEmail()+"'");
 				
-				getFriends(conector, user);
+				GetFriends.getFriends(conector, user);
 				
 				TimerTask timerTask = new MonitorFriends(user);
 		        Timer timer = new Timer(true);
@@ -49,32 +47,5 @@ public class LogIn {
 			return 2;
 		}	
 		return 1; 
-	}
-	
-	public static void getFriends(ConnectorFile conector, UserData user) throws SQLException{
-		
-		FriendData friend;
-		ArrayList<FriendData> friends = new ArrayList<FriendData>();
-
-		ResultSet rs1 = conector.SearchMySQLData("SELECT `uti2` FROM `amizade` WHERE `uti1` = '" + user.getEmail() + "'");
-
-		while (rs1.next()){					
-			friend = new FriendData(null, rs1.getString("uti2") , null, null);
-			friends.add(friend);
-
-		}
-
-		ResultSet rs2 = conector.SearchMySQLData("SELECT `uti1` FROM `amizade` WHERE `uti2` = '" + user.getEmail() + "'");
-		while (rs2.next()){
-
-			friend = new FriendData(null, rs2.getString("uti1") , null, null);
-			friends.add(friend);
-		}
-
-		Friends friendsList = new Friends(friends);
-
-		user.setFriendsList(friendsList);
-	}
-	
-	
+	}	
 }
